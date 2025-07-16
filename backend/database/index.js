f0edfbfe30ad101943e168fa03db49f62b3d4fe4
krish-vadsak45 <krishvadsak234@gmail.com>
2025-07-16 19:37:12 +0530

    credentials changes

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,16 +1,14 @@
 import mysql from "mysql2/promise";
 
 const connectDB = async () => {
-    const db = await mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "student@123",
-      database: "tms"
-    });
+  const db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASSWORD || "password",
+    database: "tms",
+  });
 
-    return db;
+  return db;
 };
 
 export default connectDB;
-
-
